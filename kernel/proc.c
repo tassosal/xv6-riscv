ab24b376a31e7a9a21cc4c19886ef83d417b9ca9
Anastasios Alevizakis <talevizakis4@gmail.com>
2025-12-22 10:31:19 +0200

    ALL TESTS PASSED & PRIO updates correctly

--- kernel/proc.c
+++ kernel/proc.c
@@ -160,22 +160,50 @@ found:
 // free a proc structure and the data hanging from it,
 // including user pages.
 // p->lock must be held.
+// static void
+// freeproc(struct proc *p)
+// {
+//   if(p->trapframe)
+//     kfree((void*)p->trapframe);
+//   p->trapframe = 0;
+//   if(p->pagetable)
+//     proc_freepagetable(p->pagetable, p->sz);
+//   p->pagetable = 0;
+//   p->sz = 0;
+//   p->pid = 0;
+//   p->parent = 0;
+//   p->name[0] = 0;
+//   p->chan = 0;
+//   p->killed = 0;
+//   p->xstate = 0;
+//   p->state = UNUSED;
+// }
+
 static void
 freeproc(struct proc *p)
 {
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
+
   p->sz = 0;
   p->pid = 0;
   p->parent = 0;
-  p->name[0] = 0;
   p->chan = 0;
   p->killed = 0;
   p->xstate = 0;
+  p->name[0] = 0;
+
+  // Clear file state
+  for(int i = 0; i < NOFILE; i++)
+    p->ofile[i] = 0;
+
+  p->cwd = 0;
+
   p->state = UNUSED;
 }
 
